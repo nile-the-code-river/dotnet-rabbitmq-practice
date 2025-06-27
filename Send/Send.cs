53f75966acd8f3f01b4aa88fb92d394105a9f2d9
Nile Kim <nile.coderiver@gmail.com>
2025-06-27 17:28:44 +0900

    implement 'send' part

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// connect to the server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+// declare a queue for us to send to (idempotent)
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+
+// publish a message to the queue
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+// When the code above finishes running, the channel and the connection will be disposed.
